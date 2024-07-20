f509cdbaec37d5771b81a4472cbda4028474bf23
shwetasng <shwetasng17@gmail.com>
2024-07-20 21:25:25 +0530

    questions for Day-4 added

--- Day3/IndexOfFirstOccurrenceInString.java
+++ Day3/IndexOfFirstOccurrenceInString.java
@@ -1,5 +1,17 @@
 package Day3;
 
-public class IndexOfFirstOccurrenceInString {
-    
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i=0;i<haystack.length()-needle.length()+1;i++){
+            // check if first letter is present
+            if(haystack.charAt(i)==needle.charAt(0)){
+                //now check if the substring matches
+                if(haystack.substring(i,needle.length()+i).equals(needle)){
+                    return i;
+                }
+            }
+            
+        }
+        return -1;
+    }
 }
