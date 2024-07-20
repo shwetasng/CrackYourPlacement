868975ee8efed61f3c0bdece8ca2a8122d6b7fe8
shwetasng <shwetasng17@gmail.com>
2024-07-20 14:50:15 +0530

    Ques-2 of Day-3 added

--- Day3/SortColors.java
+++ Day3/SortColors.java
@@ -1,5 +1,59 @@
 package Day3;
 
-public class SortColors {
-    
+// Solution-1: Brute Force will be to use any sorting algorithm for eg, Merge Sort
+
+// Solution-2: Better Solution since not using extra space
+class Solution {
+    public void sortColors(int[] nums) {
+         int count0 = 0, count1 = 0, count2 = 0;
+
+        // First pass: count the number of 0s, 1s, and 2s
+        for (int num : nums) {
+            if (num == 0) {
+                count0++;
+            } else if (num == 1) {
+                count1++;
+            } else if (num == 2) {
+                count2++;
+            }
+        }
+
+        // Second pass: overwrite the array with the counted numbers
+        int index = 0;
+        for (int i = 0; i < count0; i++) {
+            nums[index++] = 0;
+        }
+        for (int i = 0; i < count1; i++) {
+            nums[index++] = 1;
+        }
+        for (int i = 0; i < count2; i++) {
+            nums[index++] = 2;
+        }
+    }
+}
+
+// Solution-3: Optimal Solution(Dutch National Flag Algorithm)
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
