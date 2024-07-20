2ad2ed8c9c16b3b0db4cee251badbe725b8722f2
shwetasng <shwetasng17@gmail.com>
2024-07-20 21:26:05 +0530

    6th ques for day-3 added

--- Day3/IntegerToRoman.java
+++ Day3/IntegerToRoman.java
@@ -1,5 +1,22 @@
 package Day3;
 
-public class IntegerToRoman {
-    
+class Solution {
+    public String intToRoman(int num) {
+         // Define the integer values and their corresponding Roman numeral symbols
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // Iterate through the values and symbols arrays
+        for (int i = 0; i < values.length && num > 0; i++) {
+            // Append the symbol to the result as many times as the value fits into num
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        
+        return sb.toString();
+    }
 }
