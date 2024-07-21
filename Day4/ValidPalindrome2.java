82b8b59cdb1ba421b04d3b375c3fb6b7d3d64f4e
shwetasng <shwetasng17@gmail.com>
2024-07-21 23:41:53 +0530

    ques-2 of day-4 added

--- Day4/ValidPalindrome2.java
+++ Day4/ValidPalindrome2.java
@@ -1,5 +1,33 @@
 package Day4;
 
-public class ValidPalindrome2 {
-    
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
+
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right){
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+            
+        }
+        return true;
+    }
 }
