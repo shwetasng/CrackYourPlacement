f9d14eb1c9a125fd47ddbcbe76f2241069feac2b
shwetasng <shwetasng17@gmail.com>
2024-07-21 23:10:35 +0530

    ques-1 of day-4

--- Day4/ChocolateDistributionProblem.java
+++ Day4/ChocolateDistributionProblem.java
@@ -1,5 +1,21 @@
 package Day4;
 
-public class ChocolateDistributionProblem {
-    
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        if(a.size()==0){
+            return 0;
+        }
+        Collections.sort(a);
+        long res=Long.MAX_VALUE;
+        for(int i=0;i<n-m+1;i++){
+            int minElement= a.get(i);
+            int maxElement= a.get(i+m-1);
+            res=Mats.min(res,maxElement-minElement);
+        }
+        return res;
+            
+    }
 }
