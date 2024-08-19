b2e2b5de6d3c6062f359835033c9b14ef761b0af
shwetasng <shwetasng17@gmail.com>
2024-08-19 21:41:37 +0530

    DFS for graph added

---
+++ Day10/DFSGraph.java
@@ -0,0 +1,81 @@
+package Day10;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class DFSGraph {
+  private LinkedList<Integer> [] adj;
+    private int V;// NO. OF VERTICES
+    private int E;// NO. OF EDGES
+
+    // create a constructor
+    public DFSGraph(int nodes){
+        this.V=nodes;
+        this.E=0;
+        this.adj=new LinkedList[nodes];
+        for(int v=0;v<V;v++){
+            adj[v]=new LinkedList<>();// iterating for every vertex to initialize a linked list for every index of array;
+        }
+    }
+
+    public void addEdge(int u, int v){
+        adj[u].add(v);
+        adj[v].add(u);
+        E++;
+    }
+
+    // For string representation of this graph
+    public String toString(){
+        // first create a string builder
+        StringBuilder sb = new StringBuilder();
+        sb.append(V+" vertices, "+E+" edges"+"\n");
+        for(int v=0;v<V;v++){
+            sb.append(v+": ");// here we have teh vertex value now we want LL associated with this vertex and print its content
+            for(int w: adj[v]){// iterate over that vertex for the linked list associated with it
+                sb.append(w+" ");
+
+            }
+            sb.append("\n");
+        } 
+        return sb.toString();// return string representation of this string builder
+    }
+
+    public void dfs(int s){
+        // create a boolean array to keep track of visited nodes
+        boolean[] visited= new boolean[V];
+
+        // Use of Stack for DFS
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(s);
+
+        while(!stack.isEmpty()){
+            int u= stack.pop();
+            if(!visited[u]){
+                visited[u]=true;
+                System.out.print(u+" ");
+                for(int v: adj[u]){
+                    if(!visited[v]){
+                        stack.push(v);
+                    }
+                }
+            }
+        }
+        
+
+    }
+    
+    public static void main(String[] args) {
+        DFSGraph g = new DFSGraph(5);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+        g.addEdge(2, 4);
+        g.dfs(0);
+
+        // System.out.println(g);
+    }
+
+    
+}
