dc16728b2fdf88f3bd9d656eb0f02b91a957bf0a
shwetasng <shwetasng17@gmail.com>
2024-08-21 00:34:48 +0530

    reverse of LL added

--- Day11/ReverseLL.java
+++ Day11/ReverseLL.java
@@ -1,5 +1,22 @@
 package Day11;
 
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class ReverseLL {
-    
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode currnext;
+        
+        while (curr != null) {
+            currnext = curr.next;  // Update currnext before moving curr
+            curr.next = prev;       // Reverse the current node's pointer
+            prev = curr;            // Move prev to the current node
+            curr = currnext;        // Move curr to the next node (which was stored in currnext)
+        }
+        
+        return prev;  // prev is the new head of the reversed list
+        }
 }
+
