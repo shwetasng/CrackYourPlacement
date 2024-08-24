ad129a912481bd0213a24907a6861ae217285a19
shwetasng <shwetasng17@gmail.com>
2024-08-24 23:45:38 +0530

    code added

---
+++ Day12/FirstLastPosOfSortedArray.java
@@ -0,0 +1,67 @@
+package Day12;
+
+public class FirstLastPosOfSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                index = mid;
+                right = mid - 1; // Continue searching in the left half
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return index;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                index = mid;
+                left = mid + 1; // Continue searching in the right half
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        FirstLastPosOfSortedArray solution = new FirstLastPosOfSortedArray();
+        
+        // Example usage
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = solution.searchRange(nums, target);
+        System.out.println("Start: " + result[0] + ", End: " + result[1]);
+        
+        target = 6;
+        result = solution.searchRange(nums, target);
+        System.out.println("Start: " + result[0] + ", End: " + result[1]);
+    }
+}
+
